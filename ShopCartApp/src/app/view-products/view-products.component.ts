25cd964332af63cffbb58457556ff9dc1049bf80
SudheerNayak <coolsudhirnayak@gmail.com>
2023-03-26 20:11:54 +0530

    Added product

--- ShopCartApp/src/app/view-products/view-products.component.ts
+++ ShopCartApp/src/app/view-products/view-products.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ViewProductsComponent implements OnInit {
 
   products: any[];
+  showMsgDiv: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      { "productId": "P101", "productName": "Lamborghini Gallardo Spyder", "categoryId": 1, "price": 18000000, "quantityAvailable": 10 },
+      { "productId": "P102", "productName": "Ben Sherman Mens Necktie Silk Tie", "categoryId": 2, "price": 1847, "quantityAvailable": 20 },
+      { "productId": "P103", "productName": "BMW Z4", "categoryId": 1, "price": 6890000, "quantityAvailable": 10 },
+      { "productId": "P104", "productName": "Samsung Galaxy S4", "categoryId": 3, "price": 38800, "quantityAvailable": 100 }
+      ]
+    if (this.products == null) {
+      this.showMsgDiv = true;
+    }
   }
 
 }
