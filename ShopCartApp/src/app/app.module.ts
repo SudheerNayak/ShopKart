5a301fcb2cdc52967c616b8aa8d28dd967bba6d9
SudheerNayak <coolsudhirnayak@gmail.com>
2023-03-26 17:51:10 +0530

    First modification

--- ShopCartApp/src/app/app.module.ts
+++ ShopCartApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewProductsComponent
   ],
   imports: [
     BrowserModule
---
+++ ShopCartApp/src/app/view-products/view-products.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-products',
+  templateUrl: './view-products.component.html',
+  styleUrls: ['./view-products.component.css']
+})
+export class ViewProductsComponent implements OnInit {
+
+  products: any[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
