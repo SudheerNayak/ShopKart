0ed524583bc1d6c8f492fb9d2d7e8e5d18c12cbd
SudheerNayak <coolsudhirnayak@gmail.com>
2023-03-27 11:18:43 +0530

    added product

--- ShopCartApp/src/app/view-products/view-products.component.ts
+++ ShopCartApp/src/app/view-products/view-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IProduct } from '../shopCart-interfaces/product';
+import { ICategory } from '../shopCart-interfaces/category';
 @Component({
   selector: 'app-view-products',
   templateUrl: './view-products.component.html',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProductsComponent implements OnInit {
 
-  products: any[];
+  products: IProduct[];
   showMsgDiv: boolean = false;
+  categories: ICategory[];
+  filteredProducts: IProduct[];
+  imageSrc: string;
   constructor() { }
 
   ngOnInit(): void {
@@ -17,9 +21,25 @@ export class ViewProductsComponent implements OnInit {
       { "productId": "P102", "productName": "Ben Sherman Mens Necktie Silk Tie", "categoryId": 2, "price": 1847, "quantityAvailable": 20 },
       { "productId": "P103", "productName": "BMW Z4", "categoryId": 1, "price": 6890000, "quantityAvailable": 10 },
       { "productId": "P104", "productName": "Samsung Galaxy S4", "categoryId": 3, "price": 38800, "quantityAvailable": 100 }
-      ]
+    ]
+    this.categories = [
+      { "categoryId": 1, "categoryName": "Motors" },
+      { "categoryId": 2, "categoryName": "Fashion" },
+      { "categoryId": 3, "categoryName": "Electronics" }
+    ]
     if (this.products == null) {
       this.showMsgDiv = true;
+    }
+    this.filteredProducts = this.products;
+    this.imageSrc = "src/app/shopCart-images/add-item.jpg";
+  }
+  searchProductByCategory(categoryId: string) {
+    this.filteredProducts = this.products;
+    if (categoryId == "0") {
+      this.filteredProducts = this.products;
+    }
+    else {
+      this.filteredProducts = this.filteredProducts.filter(prod => prod.categoryId.toString() == categoryId);
     }
   }
 
